ccf2212f6110f31189e6d8f70d2d7d88bc2114fc
Mark Ražković <118043714+mark404k@users.noreply.github.com>
2023-03-18 18:50:08 +0400

    Script

--- main.py
+++ main.py
@@ -6,46 +6,45 @@ import time
 import random
 
 
-# URL of the webpage containing the words
+
 url = 'https://monkeytype.com/'
 
-# Words per minute
+
 wpm = 200
 
-# Calculate delay between keystrokes in seconds
 delay = random.randint(35, 85) / wpm / 5
 
-# Set up Chrome driver
+
 driver = webdriver.Chrome()
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, executable_path="path/to/executable")
 
 
-# Navigate to website
+
 driver.get(url)
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
 time.sleep(30)
 
-# Find all div elements containing words
+
 word_divs = driver.find_elements(By.CSS_SELECTOR, '.word')
 
-# Loop through each word
+
 for word_div in word_divs:
-    # Get the text content of the word div
+
     word = word_div.text
 
-    # Loop through each letter in the word
+
     for letter in word:
-        # Simulate typing the letter
+
         driver.switch_to.active_element.send_keys(letter)
         
-        # Wait for the appropriate delay between keystrokes
+
         time.sleep(delay)
 
-    # Add a space after the word
+
     driver.switch_to.active_element.send_keys(Keys.SPACE)
 
 
