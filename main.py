8890d39a8a0fa131ad7683ea17ccd174f99ca484
Mark Ražković <118043714+mark404k@users.noreply.github.com>
2023-03-19 01:01:44 +0400

    Optimized.

--- main.py
+++ main.py
@@ -2,49 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
-import random
-
-
+import time, random
 
 url = 'https://monkeytype.com/'
-
-
 wpm = 200
-
 delay = random.randint(35, 85) / wpm / 5
 
-
-driver = webdriver.Chrome()
 options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options, executable_path="path/to/executable")
-
-
-
+options.add_experimental_option('detach', True)
+driver = webdriver.Chrome(options=options, executable_path='path/to/executable')
 driver.get(url)
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
 
-time.sleep(30)
-
-
+time.sleep(15)
 word_divs = driver.find_elements(By.CSS_SELECTOR, '.word')
 
-
 for word_div in word_divs:
-
     word = word_div.text
-
-
     for letter in word:
-
         driver.switch_to.active_element.send_keys(letter)
-        
-
         time.sleep(delay)
-
-
     driver.switch_to.active_element.send_keys(Keys.SPACE)
-
-
